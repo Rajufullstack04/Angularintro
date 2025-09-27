9cb4eb12694f2e44d4f9cb8c18ece78009f3036e
Rajufullstack04 <rajufullstack777@gmail.com>
2025-09-28 00:01:25 +0530

    databinding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,35 @@ Headingname = "Raju Antharala";
  
 Fathername = "Suryanarayana";
 
-// getAdress()
-//   return  "kadiri";
-}
+ getAdress():string{
+   return  "kadiri";
+ }
+
+
+ getphoneNumber():number{
+  return 9703990371;
+ }
+
+
+// Property Binding...........................
  
+emailAdress =  "rajupower@gmail.com"
+
+ inputtype = "checkbox"
+
+//  imagelink ="src/images/img.1"
+cortoonlink = "https://www.shutterstock.com/shutterstock/photos/2644655981/display_1500/stock-vector-a-cheerful-cartoon-panda-sits-holding-a-large-red-heart-its-expression-is-joyful-and-the-style-is-2644655981.jpg"
+
+// imagesrc ="ANGULARINTRO/src/images/img.1"
+
+imagesrc ="c:\Users\rajup\OneDrive\Pictures\linked in .png"
+
+// event bindinng.............................................
 
 
+
+
+}
 
 
 
